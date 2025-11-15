4d09635fab7811d9c0940f026853d590b6d47ac9
Gergely Bencsik <becsegergely@gmail.com>
2025-11-16 00:38:01 +0100

    Further alignment to assignment 4p2

--- finder-app/finder-test.sh
+++ finder-app/finder-test.sh
@@ -36,20 +36,6 @@ rm -rf "${WRITEDIR}"
 # create $WRITEDIR if not assignment1
 assignment="assignment4-buildroot"
 
-if [ $assignment != 'assignment1' ]
-then
-	mkdir -p "$WRITEDIR"
-
-	#The WRITEDIR is in quotes because if the directory path consists of spaces, then variable substitution will consider it as multiple argument.
-	#The quotes signify that the entire string in WRITEDIR is a single string.
-	#This issue can also be resolved by using double square brackets i.e [[ ]] instead of using quotes.
-	if [ -d "$WRITEDIR" ]
-	then
-		echo "$WRITEDIR created"
-	else
-		exit 1
-	fi
-fi
 #echo "Removing the old writer utility and compiling as a native application"
 #make clean
 #make
